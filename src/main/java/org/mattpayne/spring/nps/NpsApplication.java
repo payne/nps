b4998681be6b57bdfe09c6fb1a265395c6384b24
Matt Payne <Payne@MattPayne.org>
2020-04-08 20:39:57 -0500

    Silly little form to start off.

---
+++ src/main/java/org/mattpayne/spring/nps/NpsApplication.java
@@ -0,0 +1,13 @@
+package org.mattpayne.spring.nps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mattpayne/spring/nps/NpsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mattpayne.spring.nps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
